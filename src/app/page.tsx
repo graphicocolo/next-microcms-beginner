b88cb57cb4f713c431eb853d4ce9740e9588b1e8
高橋礼 <graphicocolo@gmail.com>
2024-07-23 14:25:14 +0900

    3-2 画像を扱ってみよう

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image';
+
 import styles from '@/app/page.module.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-      <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
+      </div>
+      <Image alt="" className={styles.bgimg} height={1200} src="/img-mv.jpg" width={4000} />
+    </section>
   );
 };
 
