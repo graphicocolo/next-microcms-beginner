12f51611fbbf684b6696f2ef69b6e30e720d8566
高橋礼 <graphicocolo@gmail.com>
2024-08-11 15:10:53 +0900

    7-4-5 useSearchParamsを使用したときの注意点

--- src/app/_components/SearchField/index.tsx
+++ src/app/_components/SearchField/index.tsx
@@ -1,12 +1,18 @@
-'use client';
+'use client'; // ユーザーの操作によって UI を更新したい場合などは、クライアントコンポーネントを利用する必要がある
 
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 import styles from '@/app/_components/SearchField/index.module.css';
 
-const SearchField = () => {
+const SearchFieldComponent = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  // handleSubmit の処理の流れ
+  // フォームイベントのデフォルト動作をキャンセル
+  // 入力された値を取得
+  // 検索ページに遷移
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // フォームデフォルト動作をキャンセル（サーバーへフォームの内容を送信しない）
     const q = e.currentTarget.elements.namedItem('q'); // input要素を取得
@@ -17,16 +23,30 @@ const SearchField = () => {
       // qという名前のパラメータに qの値をセット（空白を削除した上で）
       // /news/search?q=ユーザーが入力した値 というURLに遷移させるための準備
       params.set('q', q.value.trim());
-      router.push(`/news/search?&{params.toString()}`); // pushメソッドでページ遷移
+      router.push(`/news/search?${params.toString()}`); // pushメソッドでページ遷移
     }
   };
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <label className={styles.search}>
         <Image alt="検索" height={16} loading="eager" src="/search.svg" width={16} />
-        <input className={styles.searchInput} name="q" placeholder="キーワードを入力" type="text" />
+        <input
+          className={styles.searchInput}
+          defaultValue={searchParams.get('q') ?? undefined}
+          name="q"
+          placeholder="キーワードを入力"
+          type="text"
+        />
       </label>
     </form>
+  );
+};
+
+const SearchField = () => {
+  return (
+    <Suspense>
+      <SearchFieldComponent />
+    </Suspense>
   );
 };
 
